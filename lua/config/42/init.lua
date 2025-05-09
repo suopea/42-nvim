3d7132f4cdd0200bc6e4cf9bee0901022f2e1782
suopea <suopea@pm.me>
2025-05-09 15:12:36 +0300

    Initial configuration.

--- lua/config/42/init.lua
+++ lua/config/42/init.lua
@@ -3,13 +3,13 @@
 -- NOTE: If you are from 42, **you might want to edit these** :)
 --
 -- 42 Global Variables.
-vim.g.user42 = 'SET YOUR USER UP'
-vim.g.mail42 = 'SET YOUR EMAIL UP'
+vim.g.user42 = 'ssuopea'
+vim.g.mail42 = 'ssuopea@student.hive.fi'
 
 require("norminette").setup({
 	runOnSave = true,
 	maxErrorsToShow = 15,
 	-- NOTE: Edit this to true if you want integrated Normiette!
 	-- Make sure to have it installed first (eg. with pip)
-	active = false,
+	active = true,
 })
